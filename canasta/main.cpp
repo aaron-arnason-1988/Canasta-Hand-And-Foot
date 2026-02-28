87050f67a8795798e28b42c50992d1375b7487d4
Aaron <aaron.arnason.1988@gmail.com>
2026-02-28 14:52:40 -0700

    Added toString method for Card

--- canasta/main.cpp
+++ canasta/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
+#include <array>
 
 // Card data (Standard)
 // Suit Definition
-enum class Suit { None, Hearts, Diamond, Clubs, Spades };
+enum class Suit { None, Hearts, Diamonds, Clubs, Spades };
 
 // Rank Definition
 enum class Rank { 
@@ -24,33 +26,38 @@ struct Card {
 		rank = r;
 	}
 
+	// This toStrings the card in the format
+	// [Card]	Rank of Suit
+	// Uses string/array stdlibc++
 	std::string toString() const {
-		std::string rankString;
-		std::string suitString;
 
-		static const std::string rankStrings[] = {
+		std::string cardToString = "[Card]\t";	
+		
+		cardToString += std::array<std::string, 14>{
 			"Joker", "Ace",   "Two",  "Three", "Four", "Five",  "Six",
 			"Seven", "Eight", "Nine", "Ten",   "Jack", "Queen", "King"
-		};
-		static const std::string suitStrings[] = {
-			"None", "Hearts", "Diamonds", "Clubs", "Spades"
-		};
-
-
-
-		int rankIndex = static_cast<int>(rank);
-		int suitIndex = static_cast<int>(suit);
-
-		if (rank == Rank::Joker) return rankStrings[rankIndex];
-		return rankStrings[rankIndex] + " of " + suitStrings[suitIndex] + "\n";
+		}[static_cast<int>(rank)];
+		
+		if (rank == Rank::Joker) {
+			return cardToString + "\n";
+		} else {
+			return cardToString + " of " + std::array<std::string, 5>{
+				"None", "Hearts", "Diamonds", "Clubs", "Spades"
+			}[static_cast<int>(suit)] + "\n";
+		}
 	}
 };
 
 
 int main() {
-	Card c(Rank::Ace, Suit::Spades);
+	Card c(Rank::Joker, Suit::None);
+	Card c2(Rank::Ace, Suit::Hearts);
+	Card s4(Rank::Four, Suit::Spades);
+	Card c9(Rank::Nine, Suit::Clubs);
 
-
-	std::cout << c.toString() << "\n";
+	std::cout << c.toString();
+	std::cout << c2.toString();
+	std::cout << s4.toString();
+	std::cout << c9.toString();
 	return 0;
 }
