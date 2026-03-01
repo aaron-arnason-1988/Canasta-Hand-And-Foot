8daac9bff1f9046329446c8c7f4b40b4998dfe6d
Aaron <aaron.arnason.1988@gmail.com>
2026-03-01 01:39:53 -0700

    main.cpp now has a valid 52 card deck plus 2 jokers, next step create deck class.

--- canasta/main.cpp
+++ canasta/main.cpp
@@ -1,16 +1,45 @@
+#include <vector>
+#include <array>
+
 #include "libraries/card.h"	// -I libraries if only #include "card.h"
 
 
 int main() {
-	Card c(Card::Rank::Joker, Card::Suit::None);
-	Card c2(Card::Rank::Ace, Card::Suit::Hearts);
-	Card s4(Card::Rank::Four, Card::Suit::Spades);
-	Card c9(Card::Rank::Nine, Card::Suit::Clubs);
 
-	c.printCard();
-	c2.printCard();
-	s4.printCard();
-	c9.printCard();
+	// This represents a deck of cards
+	std::vector<Card> deck;
+	
+	// This is an array representing all possible ranks
+	constexpr std::array<Card::Rank, 13> allRanks {
+		Card::Rank::Ace,  Card::Rank::Two, Card::Rank::Three, Card::Rank::Four,
+		Card::Rank::Five, Card::Rank::Six, Card::Rank::Seven, Card::Rank::Eight,
+		Card::Rank::Nine, Card::Rank::Ten, Card::Rank::Jack,  Card::Rank::Queen, 
+		Card::Rank::King
+	};
+
+	// This is an array representing all possible suits
+	constexpr std::array<Card::Suit, 4> allSuits {
+		Card::Suit::Hearts, Card::Suit::Clubs, Card::Suit::Diamonds, Card::Suit::Spades
+	};
+
+
+	// Each set of cards has 13 ranks and 4 suits, this creates 52 cards
+	for (auto suit : allSuits) {
+		for (auto rank : allRanks) {
+			deck.emplace_back(rank, suit);
+		}
+	}
+
+	// Each set of cards requires 2 jokers.
+	for (int i = 0; i < 2; i++) {
+		deck.emplace_back(Card::Rank::Joker, Card::Suit::None);
+	}
+
+
+	// This prints every card added to the deck
+	for (Card& c : deck) {
+		c.printCard();
+	}
 
 	return 0;
 }
